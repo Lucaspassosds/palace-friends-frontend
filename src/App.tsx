f3d6b567b8a86e1dbbc3fa8ecfe931e8fc90fb68
Lucas Siqueira <lsiqueira355@gmail.com>
2021-03-29 01:10:11 -0300

    more dependencies and config

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,45 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './css/App.css';
+import { PlayerContext } from './context/context';
+import CreateGame from './components/CreateGame.component';
+import PalaceGame from './components/PalaceGame.component';
+import { GameMode } from './models/Game';
 
 function App() {
+
+  const [didRedirect, setDidRedirect] = React.useState(false)
+
+  const playerDidRedirect = React.useCallback(() => {
+    setDidRedirect(true)
+  }, [])
+
+  const playerDidNotRedirect = React.useCallback(() => {
+    setDidRedirect(false)
+  }, [])
+
+  const [userName, setUserName] = React.useState('')
+
   return (
-    <Router>
-      <div className="App">
-        <nav className='navbar navbar-expand-lg navbar-dark bg-dark justify-content-center fixed-top'>
-          <Link to={"/"} className='navbar-brand'>
-            <p className="main-text">Palace</p>
-          </Link>
-        </nav>
-      </div>
-    </Router>
+    <PlayerContext.Provider value={{ didRedirect: didRedirect, playerDidRedirect: playerDidRedirect, playerDidNotRedirect: playerDidNotRedirect }}>
+      <Router>
+        <div className="App">
+          <nav className='navbar navbar-expand-lg navbar-dark bg-dark justify-content-center col-md-12'>
+            <Link to={"/"} className='navbar-brand'>
+              <p className="main-text">Palace</p>
+            </Link>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <CreateGame setUserName={setUserName} />
+          </Route>
+          <Route exact path="/game/:gameid">
+            <PalaceGame myUserName={userName} gamemode={GameMode.TWO_PLAYERS} />
+          </Route>
+        </Switch>
+      </Router>
+    </PlayerContext.Provider >
   );
 }
 
---
+++ src/context/context.ts
@@ -0,0 +1,8 @@
+
+import { createContext } from 'react'
+
+export const PlayerContext = createContext({
+    didRedirect: false, 
+    playerDidRedirect: () => {}, 
+    playerDidNotRedirect: () => {}
+})
