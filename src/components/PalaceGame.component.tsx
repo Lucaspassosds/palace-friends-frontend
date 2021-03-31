fd5bdfe90d021e5b5f546724646db0fb77582da2
Lucas Siqueira <lsiqueira355@gmail.com>
2021-03-30 23:33:31 -0300

    play card functionality

--- src/components/PalaceGame.component.tsx
+++ src/components/PalaceGame.component.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Game, { GameMode } from "../models/Game";
 import { Card } from "../models/Deck";
+import requireImage from "../utils/imageImportHelper";
 
 interface PalaceGameProps {
     gamemode: GameMode;
@@ -15,15 +16,16 @@ interface SelectedCardInfo {
 const PalaceGame: React.FC<PalaceGameProps> = (props) => {
     const [game, setGame] = useState(new Game(props.gamemode));
     const [selectedCards, setSelectedCards] = useState<SelectedCardInfo[]>([]);
+    //console.log(game);
 
-    const generateCards = () => {
-        console.log(game);
+    const generatePlayerCards = () => {
         return game.players.map((player, index) => {
             return (
-                <div>
+                <div key={index}>
                     <h1>Player {index + 1} cards</h1>
-                    {player.getDeck().map((card, cardIndex) => {
-                        const bg = require("../assets/img/club_1.png").default;
+                    {player.current.map((card, cardIndex) => {
+                        const bg = requireImage(`${card.suit}_${card.value}`);
+
                         return (
                             <img
                                 key={cardIndex}
@@ -39,35 +41,71 @@ const PalaceGame: React.FC<PalaceGameProps> = (props) => {
         });
     };
 
-    const selectCard = (playerIndex: number, cardIndex: number, imageUrl : string) => {
+    const selectCard = (
+        playerIndex: number,
+        cardIndex: number,
+        imageUrl: string
+    ) => {
         //if player da vez
         //if cartas validas serem jogadas juntas
-        setSelectedCards([...selectedCards, { playerIndex, cardIndex, imageUrl }]);
+
+        //caso a carta ja tenha sido selecionada
+        if (selectedCards.some((value) => value.cardIndex === cardIndex)) {
+            //caso a carta ja tenha sido, remover a carta
+            setSelectedCards(
+                selectedCards.filter((value) => value.cardIndex !== cardIndex)
+            );
+        } else {
+            //caso ainda nao tenha sido, adiciona
+            setSelectedCards([
+                ...selectedCards,
+                { playerIndex, cardIndex, imageUrl },
+            ]);
+        }
     };
 
     const play = () => {
         selectedCards.forEach((card, index) => {
             /**
-             * chama metodo de Game que vai: 
+             * chama metodo de Game que vai:
              * 1. remover a carta do baralho do jogador que a jogou.
              * 2. jogar a carta no jogo(aplicando as logicas necessarias)
-             * 3. adicionar imagem da carta na div de pilha
              */
-            const cardImg = document.createElement('img');
-            cardImg.setAttribute('src', card.imageUrl);
-            cardImg.setAttribute('alt', 'Oops! :(');
-            cardImg.setAttribute('class', 'player-card');
-            document.getElementById('pile')?.appendChild(cardImg);
+            game.turn(card.playerIndex, card.cardIndex);
         });
         setSelectedCards([]);
+    };
+
+    const generatePileCards = () => {
+        return game.gamePile.deck.map((card, index) => {
+            const imgUrl = requireImage(`${card.suit}_${card.value}`);
+            return (
+                <img
+                    src={imgUrl}
+                    alt='Oops! :('
+                    className='player-card'
+                    key={index}
+                    style={{
+                        position: "relative",
+                        zIndex: index + 1,
+                        marginLeft: "-4.25vh",
+                    }}
+                />
+            );
+        });
     };
 
     return (
         <div className='container game-container'>
             <div id='game-table'>
-                {generateCards()}
-                <div id='pile'>PILE OF CARDS!</div>
-                <button className='btn btn-primary play-btn' onClick={() => play()}>PLAY</button>
+                {generatePlayerCards()}
+                <div id='pile'>{generatePileCards()}</div>
+                <button
+                    className='btn btn-primary play-btn'
+                    onClick={() => play()}
+                >
+                    PLAY
+                </button>
             </div>
         </div>
     );
